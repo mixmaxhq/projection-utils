e4bac797f282aa013cf19fd93eb7bef4320bc561
Eli Skeggs <eli@mixmax.com>
2020-02-03 17:42:21 -0800

    style: apply prettier

--- test/field-set.test.js
+++ test/field-set.test.js
@@ -1,4 +1,4 @@
-const {ProjectionFieldSet} = require('..');
+const { ProjectionFieldSet } = require('..');
 
 // We want to ensure that intersect and union never unintentionally return one
 // of their operands: they should be pure functions.
@@ -14,7 +14,7 @@ function instanceInvariant(fn) {
 describe('ProjectionFieldSet', () => {
   // Set up invariant checks.
   beforeAll(() => {
-    const {intersect, union} = ProjectionFieldSet.prototype;
+    const { intersect, union } = ProjectionFieldSet.prototype;
     Object.assign(ProjectionFieldSet.prototype, {
       intersect: instanceInvariant(intersect),
       union: instanceInvariant(union),
@@ -25,10 +25,7 @@ describe('ProjectionFieldSet', () => {
     const fields = new ProjectionFieldSet([['users', 'id'], ['content']]);
     const allFields = [...fields];
     expect(allFields).toHaveLength(2);
-    expect(allFields).toEqual(expect.arrayContaining([
-      ['content'],
-      ['users', 'id'],
-    ]));
+    expect(allFields).toEqual(expect.arrayContaining([['content'], ['users', 'id']]));
   });
 
   it('should dedupe', () => {
@@ -40,55 +37,78 @@ describe('ProjectionFieldSet', () => {
     const fields = ProjectionFieldSet.fromDotted(['users.id', 'users.email', 'content']);
     const allFields = [...fields];
     expect(allFields).toHaveLength(3);
-    expect(allFields).toEqual(expect.arrayContaining([
-      ['content'],
-      ['users', 'email'],
-      ['users', 'id'],
-    ]));
+    expect(allFields).toEqual(
+      expect.arrayContaining([['content'], ['users', 'email'], ['users', 'id']])
+    );
   });
 
   it('should intersect field sets', () => {
-    const permittedFields = ProjectionFieldSet.fromDotted(
-      ['users.id', 'users.email', 'share', 'notifications', 'content']);
+    const permittedFields = ProjectionFieldSet.fromDotted([
+      'users.id',
+      'users.email',
+      'share',
+      'notifications',
+      'content',
+    ]);
 
-    const desiredFields = ProjectionFieldSet.fromDotted(
-      ['users', 'users.accessToken', 'share', 'notifications.count', 'invalid']);
+    const desiredFields = ProjectionFieldSet.fromDotted([
+      'users',
+      'users.accessToken',
+      'share',
+      'notifications.count',
+      'invalid',
+    ]);
 
     // The fields we want, where they're permitted.
     const selectedFields = permittedFields.intersect(desiredFields);
     const allSelected = [...selectedFields];
     expect(allSelected).toHaveLength(4);
-    expect(allSelected).toEqual(expect.arrayContaining([
-      ['notifications', 'count'],
-      ['share'],
-      ['users', 'email'],
-      ['users', 'id'],
-    ]));
+    expect(allSelected).toEqual(
+      expect.arrayContaining([
+        ['notifications', 'count'],
+        ['share'],
+        ['users', 'email'],
+        ['users', 'id'],
+      ])
+    );
   });
 
   it('should union field sets', () => {
-    const selectedFields = ProjectionFieldSet.fromDotted(
-      ['users.id', 'users.email', 'notifications.count', 'share']);
+    const selectedFields = ProjectionFieldSet.fromDotted([
+      'users.id',
+      'users.email',
+      'notifications.count',
+      'share',
+    ]);
 
-    const mandatoryFields = ProjectionFieldSet.fromDotted(
-      ['internalVersion', 'notifications.targets', 'share']);
+    const mandatoryFields = ProjectionFieldSet.fromDotted([
+      'internalVersion',
+      'notifications.targets',
+      'share',
+    ]);
 
     const queryFields = selectedFields.union(mandatoryFields);
     const allQuery = [...queryFields];
     expect(allQuery).toHaveLength(6);
-    expect(allQuery).toEqual(expect.arrayContaining([
-      ['internalVersion'],
-      ['notifications', 'count'],
-      ['notifications', 'targets'],
-      ['share'],
-      ['users', 'email'],
-      ['users', 'id'],
-    ]));
+    expect(allQuery).toEqual(
+      expect.arrayContaining([
+        ['internalVersion'],
+        ['notifications', 'count'],
+        ['notifications', 'targets'],
+        ['share'],
+        ['users', 'email'],
+        ['users', 'id'],
+      ])
+    );
   });
 
   it('should produce a mongo projection', () => {
-    const selectedFields = ProjectionFieldSet.fromDotted(
-      ['users.id', 'users.email', 'notifications.count', 'share']);
+    const selectedFields = ProjectionFieldSet.fromDotted([
+      'users.id',
+      'users.email',
+      'notifications.count',
+      'share',
+    ]);
 
     expect(selectedFields.toMongo()).toEqual({
       'users.id': 1,
@@ -106,21 +126,26 @@ describe('ProjectionFieldSet', () => {
   });
 
   it('should produce dot-joined fields', () => {
-    const selectedFields = ProjectionFieldSet.fromDotted(
-      ['users.id', 'users.email', 'notifications.count', 'share']);
-    const dotted = [...selectedFields.toDotted()];
-    expect(dotted).toHaveLength(4);
-    expect(dotted).toEqual(expect.arrayContaining([
+    const selectedFields = ProjectionFieldSet.fromDotted([
       'users.id',
       'users.email',
       'notifications.count',
       'share',
-    ]));
+    ]);
+    const dotted = [...selectedFields.toDotted()];
+    expect(dotted).toHaveLength(4);
+    expect(dotted).toEqual(
+      expect.arrayContaining(['users.id', 'users.email', 'notifications.count', 'share'])
+    );
   });
 
   it('should widen with the given field', () => {
-    const selectedFields = ProjectionFieldSet.fromDotted(
-      ['users.id', 'users.email', 'notifications.count', 'share']);
+    const selectedFields = ProjectionFieldSet.fromDotted([
+      'users.id',
+      'users.email',
+      'notifications.count',
+      'share',
+    ]);
     selectedFields.widen(['notifications', 'targets', 'value']);
 
     expect(selectedFields.toMongo()).toEqual({
@@ -157,8 +182,11 @@ describe('ProjectionFieldSet', () => {
   });
 
   it('should handle operations on special sets', () => {
-    const mandatoryFields = ProjectionFieldSet.fromDotted(
-      ['internalVersion', 'notifications.targets', 'share']);
+    const mandatoryFields = ProjectionFieldSet.fromDotted([
+      'internalVersion',
+      'notifications.targets',
+      'share',
+    ]);
     const singular = new ProjectionFieldSet([[]]);
     const empty = new ProjectionFieldSet([]);
 
@@ -179,8 +207,11 @@ describe('ProjectionFieldSet', () => {
   });
 
   it('should determine containment', () => {
-    const mandatoryFields = ProjectionFieldSet.fromDotted(
-      ['internalVersion', 'notifications.targets', 'share']);
+    const mandatoryFields = ProjectionFieldSet.fromDotted([
+      'internalVersion',
+      'notifications.targets',
+      'share',
+    ]);
     expect(mandatoryFields.contains([])).toBe(false);
     expect(mandatoryFields.contains(['internalVersion'])).toBe(true);
     expect(mandatoryFields.contains(['notifications'])).toBe(false);
@@ -194,8 +225,11 @@ describe('ProjectionFieldSet', () => {
   });
 
   fit('should produce contained entries', () => {
-    const mandatoryFields = ProjectionFieldSet.fromDotted(
-      ['internalVersion', 'notifications.targets', 'share']);
+    const mandatoryFields = ProjectionFieldSet.fromDotted([
+      'internalVersion',
+      'notifications.targets',
+      'share',
+    ]);
     expect([...mandatoryFields.get([])]).toEqual([...mandatoryFields.entries()]);
     expect([...mandatoryFields.get([], false)]).toEqual([...mandatoryFields.entries()]);
     expect([...mandatoryFields.getDotted('')]).toEqual([...mandatoryFields.toDotted()]);
